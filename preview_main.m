f6d68221cee985def376a3ac78521eef37977845
迪南 林 <eamdfan@hotmail.com>
2025-05-07 18:34:37 +0800

    enhance analyze_temperature_points: - add  as 6th argument (default '特征值') - refactor data‐extraction to use user‐specified subfolder - maintain backward compatibility with previous calls

--- preview_main.m
+++ preview_main.m
@@ -1,5 +1,5 @@
 tic;
 %preview_csv('2020-08-24\2020-08-24.csv', 100);
 %preview_csv('F:\管柄大桥健康监测数据\2025-03-27\波形\GB-RSG-G05-001-01.csv', 20);
-preview_csv('G:\BaiduNetdiskDownload\管柄大桥数据\2025-03-25\波形\GB-DIS-P04-001-01-X.csv', 500);
+preview_csv('G:\BaiduNetdiskDownload\管柄大桥数据\2025-03-25\波形\GB-DIS-P04-001-01-X.csv', 300);
 toc;
--- test_main.m
+++ test_main.m
@@ -4,11 +4,11 @@
 %batch_remove_header('G:\\BaiduNetdiskDownload\\管柄大桥数据', '2025-02-26', '2025-03-25',true);
 %batch_resample_data_parallel('G:\\BaiduNetdiskDownload\\管柄大桥数据', '2025-02-26', '2025-03-25', 100,true,'batch_resample_data_parallel_config.csv');
 
-%plot_temperature_point_curve('G:\\BaiduNetdiskDownload\\管柄大桥数据','GB-RTS-G05-001-01','2025-02-26', '2025-03-25')
 
-%root = 'G:/BaiduNetdiskDownload/管柄大桥数据';
-%pts = {'GB-RTS-G05-001-01','GB-RTS-G05-001-02','GB-RTS-G05-001-03'};
-%analyze_temperature_points(root, pts, '2025-02-26','2025-03-25','temp_stats.xlsx');
+
+root = 'G:/BaiduNetdiskDownload/管柄大桥数据';
+pts = {'GB-RTS-G05-001-01','GB-RTS-G05-001-02','GB-RTS-G05-001-03'};
+analyze_temperature_points(root, pts, '2025-02-26','2025-03-25','temp_stats.xlsx','特征值_重采样');
 
 %root = 'G:/BaiduNetdiskDownload/管柄大桥数据/';
 %pts  = {'GB-RHS-G05-001-01','GB-RHS-G05-001-02','GB-RHS-G05-001-03'};
@@ -31,6 +31,6 @@
 %analyze_crack_points(root, '2025-02-26','2025-03-25','crack_stats.xlsx','特征值')
 %toc;
 
-root = 'G:/BaiduNetdiskDownload/管柄大桥数据/';
-analyze_strain_points(root, '2025-02-26','2025-03-25','strain_stats.xlsx','特征值')
+%root = 'G:/BaiduNetdiskDownload/管柄大桥数据/';
+%analyze_strain_points(root, '2025-02-26','2025-03-25','strain_stats.xlsx','特征值')
 
